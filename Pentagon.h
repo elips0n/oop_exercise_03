a2bb90d2a504ab763c951451ce3e92c5eb1fbae8
elips0n <55798420+elips0n@users.noreply.github.com>
2020-05-20 17:13:45 +0300

    Create Pentagon.h

---
+++ Pentagon.h
@@ -0,0 +1,13 @@
+#ifndef OOP_HELP_PENTAGON_H
+#define OOP_HELP_PENTAGON_H
+#include "Figure.h"
+
+class Pentagon : public Figure{
+public:
+    explicit Pentagon(std::vector<std::pair<double, double>> nVertex, std::string me) : Figure(std::move(nVertex), std::move(me)) {}
+    std::pair<double, double> center() override;
+    std::vector <std::pair <double, double>> get_vertex() override;
+    double square() override;
+    std::string who_i_am() override;
+};
+#endif //OOP_HELP_PENTAGON_H
