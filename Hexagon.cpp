011e7d89fd09b99c10d3efa937006189d6df82dd
elips0n <55798420+elips0n@users.noreply.github.com>
2020-05-20 17:15:21 +0300

    Create Hexagon.cpp

---
+++ Hexagon.cpp
@@ -0,0 +1,34 @@
+#include "Hexagon.h"
+
+std::string Hexagon::who_i_am() {
+    return this->i_am;
+}
+
+
+std::pair<double, double> Hexagon::center(){
+    std::pair<double, double> answer(0, 0);
+    for(auto i : vertex){
+        answer.first += i.first;
+        answer.second += i.second;
+    }
+    answer.first /= vertex.size();
+    answer.second /= vertex.size();
+    return answer;
+}
+
+std::vector <std::pair <double, double>> Hexagon::get_vertex(){
+    return this->vertex;
+}
+
+double Hexagon::square(){
+    double res = 0;
+    std::pair <double, double> point2;
+    std::pair <double, double> point1 = vertex[0];
+    for(int i = 1; i < vertex.size();++i){
+        point2 = vertex[i];
+        res += (point1.first + point2.first) * (point2.second - point1.second);
+        point1 = point2;
+    }
+    res += (vertex[0].first + vertex[vertex.size() - 1].first) * (vertex[0].second - vertex[vertex.size() - 1].second);
+    return std::abs(res) / 2;
+}
