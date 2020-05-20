3a9ea484404712656615a59a1e2c2a37eb77c2b8
elips0n <55798420+elips0n@users.noreply.github.com>
2020-05-20 17:12:31 +0300

    Create Figure.h

---
+++ Figure.h
@@ -0,0 +1,25 @@
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <string>
+
+#ifndef OOP_HELP_FIGURE_H
+#define OOP_HELP_FIGURE_H
+
+class Figure{
+public:
+    explicit Figure(std::vector <std::pair <double, double>> n_vertex, std::string n_i_am){
+        vertex = std::move(n_vertex);
+        i_am = std::move(n_i_am);
+    }
+    virtual std::pair<double, double> center() = 0;
+    virtual std::vector <std::pair <double, double>> get_vertex() = 0;
+    virtual double square() = 0;
+    virtual std::string who_i_am() = 0;
+
+protected:
+    std::vector <std::pair <double, double>> vertex;
+    std::string i_am;
+};
+
+#endif //OOP_HELP_FIGURE_H
